8d64e44ddc1a306ebaafdb02a2ea97b753cc3d9d
Jacek Generowicz <jacg@my-post-office.net>
2021-02-27 15:02:43 +0100

    Add first working version, using nightly rust

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
+    mozillapkgs = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, mozillapkgs }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+
+      # Get a specific rust version
+      mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") {};
+      rust = (mozilla.rustChannelOf {
+        date = "2021-02-26"; # get the current date with `date -I`
+        channel = "nightly";
+        sha256 = "sha256-hTj47PwUeP276uF6+HLDzsHYoDvfJa+y9o+vmxZqV0g=";
+      }).rust;
+
+      # Override the version used in naersk
+      naersk-lib = naersk.lib."${system}".override {
+        cargo = rust;
+        rustc = rust;
+      };
+    in rec {
+      # `nix build`
+      packages.my-project = naersk-lib.buildPackage {
+        pname = "my-project";
+        root = ./.;
+      };
+      defaultPackage = packages.my-project;
+
+      # `nix run`
+      apps.my-project = utils.lib.mkApp {
+        drv = packages.my-project;
+      };
+      defaultApp = apps.my-project;
+
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        # supply the specific rust version
+        nativeBuildInputs = [ rust ];
+      };
+    });
+}
